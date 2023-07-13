75e23f4745db9eebca241618a733b219ec9facef
Ujjawal kumar <ujjawalkr67@gmail.com>
2023-07-13 21:29:46 +0530

    initial commit

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+<App />
+</BrowserRouter>
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>KURUKSHETRA</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+			</nav>
+		</div>
+	);
+};
+
+export default Main;
