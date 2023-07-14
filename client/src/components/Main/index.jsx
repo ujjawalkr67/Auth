75058d26dfc7f25961b0d68e18814e4347a07e80
Ujjawal kumar <ujjawalkr67@gmail.com>
2023-07-14 16:03:12 +0530

    all

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-<App />
-</BrowserRouter>
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
